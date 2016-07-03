f2d3c9f964653327245c1e42a4cb27cce6635206
Brayden Li <brayden.li@ef.com>
2016-07-03 20:07:17 +0800

    wait until remote port is ready

--- src/entry.sh
+++ src/entry.sh
@@ -1,3 +1,10 @@
 #! /usr/bin/env bash
+
+nc -z -w 1 rabbitmq-server 5672
+while [ $? -ne 0 ]; do
+	sleep 1;
+    nc -z -w 1 rabbitmq-server 5672
+done
+
 [ $# -ne 1 ] && { echo "one argument required: Send or Recv"; exit 1; }
 java -cp .:commons-io-1.2.jar:commons-cli-1.1.jar:rabbitmq-client.jar $1 
