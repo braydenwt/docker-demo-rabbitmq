e9d94610372081edb235ab436b9573d6ea227dba
Brayden Li <brayden.wt@gmail.com>
2016-07-04 14:12:05 +0800

    Update entry.sh

--- src/entry.sh
+++ src/entry.sh
@@ -2,8 +2,8 @@
 
 nc -z -w 1 rabbitmq-server 5672
 while [ $? -ne 0 ]; do
-	sleep 1;
-    nc -z -w 1 rabbitmq-server 5672
+  sleep 1;
+  nc -z -w 1 rabbitmq-server 5672
 done
 
 [ $# -ne 1 ] && { echo "one argument required: Send or Recv"; exit 1; }
